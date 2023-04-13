9122db7169fe27437d6680e1e376aba8873f59ac
Harsh-2909 <harsh29092000@gmail.com>
2023-04-13 22:07:48 +0530

    feat: Added skeleton for jobs api express

---
+++ jobs-api-express/middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const { UnauthorizedError } = require("../errors");
+
+const authenticator = async (req, res, next) => {
+    const { authorization: authHeader } = req.headers;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new UnauthorizedError("Invalid credentials to access this route");
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const { id, username } = decoded;
+        req.user = { id, username };
+    } catch (error) {
+        throw new UnauthorizedError("Invalid credentials to access this route");
+    }
+    next();
+};
+
+module.exports = authenticator;
---
+++ jobs-api-express/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+// const { login, dashboard } = require("../controllers/main");
+const authenticator = require("../middlewares/auth");
+const router = express.Router({ mergeParams: true });
+
+// router.post("/login", login);
+// router.get("/dashboard", authenticator, dashboard);
+
+module.exports = router;
