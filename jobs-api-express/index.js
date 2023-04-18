2118d97563b173f0f8fdfbc619a7e42019529264
Harsh-2909 <harsh29092000@gmail.com>
2023-04-18 19:11:58 +0530

    feat: Added more security by adding middlewares

--- jobs-api-express/index.js
+++ jobs-api-express/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const expressAsyncErrors = require("express-async-errors");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 const router = require("./routes");
 const connectToMongo = require('./db');
 const notFoundMiddleware = require("./middlewares/not_found");
@@ -10,10 +14,21 @@ const errorMiddleware = require("./middlewares/error_handler");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV.toUpperCase() || "DEVELOPMENT";
+const morgan_mode = ENV === "PRODUCTION" ? "tiny" : "dev";
 
 // Middlewares
+app.set("trust proxy", 1);
+app.use(rateLimiter({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+
+}));
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan(morgan_mode));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // API Routes
 app.use("/api/v1", router);
